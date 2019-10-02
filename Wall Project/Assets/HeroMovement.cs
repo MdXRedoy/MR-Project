1c63c2bf050c2a81a961e20d9410c0ddb7c936a7
Md Redoy <rdy6792@gmail.com>
2019-10-02 10:11:43 -0400

    more added

--- Wall Project/Assets/HeroMovement.cs
+++ Wall Project/Assets/HeroMovement.cs
@@ -11,19 +11,19 @@ public class HeroMovement : MonoBehaviour
     {
         if ( Input.GetKey(KeyCode.W))
         {
-            transform.position += new Vector3(0, 2 * Time.deltaTime * Speed, 1);
+            transform.position += new Vector3(0, 2, 1); //2 * Time.deltaTime * Speed, 1);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += new Vector3(0, -2* Time.deltaTime * Speed, 1);
+            transform.position += new Vector3(0, -2, 1); //-2* Time.deltaTime * Speed, 1);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(2 * Time.deltaTime * Speed, -0, 1);
+            transform.position += new Vector3(2, -0, 1);//2 * Time.deltaTime * Speed, -0, 1);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += new Vector3(-2 * Time.deltaTime * Speed, 0, 1);
+            transform.position += new Vector3(-2, 0, 1);//-2 * Time.deltaTime * Speed, 0, 1);
         }
     }
 }
